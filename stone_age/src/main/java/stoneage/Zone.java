32af320b72295a39038ea4ce95bc00184ede8dfa
Mohamed06000 <mazymohamed06@gmail.com>
2019-11-03 00:06:52 +0100

    debut de javadoc

--- stone_age/src/main/java/stoneage/Zone.java
+++ stone_age/src/main/java/stoneage/Zone.java
@@ -4,22 +4,34 @@ public class Zone {
 
     private int nbOuvrierSurZone = 0;
 
+    /**
+     * Recupérer le  nombre d'ouvrier sur la zone
+     * @return
+     */
     public int getNbOuvrierSurZone() {
         return nbOuvrierSurZone;
     }
 
+    /**
+     * Assigner un nombre d'ouvier sur la zone
+     * @param nbOuvrierSurZone
+     */
     public void setNbOuvrierSurZone(int nbOuvrierSurZone) {
         this.nbOuvrierSurZone = nbOuvrierSurZone;
     }
 
+    /**
+     * Placer un nombre d'ouvrier sur la zone
+     * @param nbOuvrierAplacer
+     */
     public void placeOuvrier(int nbOuvrierAplacer){
         nbOuvrierSurZone += nbOuvrierAplacer;
     }
 
+    /**
+     * retirer un nombre d'ouvrier de la zone
+     * @param nbOuvrierRetirer
+     */
     public void retirerOuvrier(int nbOuvrierRetirer) { nbOuvrierSurZone -= nbOuvrierRetirer; }
 
-    public void recuperation(Inventaire inventaireJoueur){
-        inventaireJoueur.setNbRessource(inventaireJoueur.getNbRessource() + 1);
-
-    }
 }
