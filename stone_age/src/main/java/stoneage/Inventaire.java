8accfe3c3eac2e23bcc2c08f1dcb4f22e5ccd21d
Mohamed06000 <mazymohamed06@gmail.com>
2019-11-03 00:30:59 +0100

    Class Inventaire definited

--- stone_age/src/main/java/stoneage/Inventaire.java
+++ stone_age/src/main/java/stoneage/Inventaire.java
@@ -1,33 +1,67 @@
 package stoneage;
 
+/**
+ * Classe de l'inventaire du joueur
+ */
 public class Inventaire {
     private int nbOuvrier;
     private int nbRessource = 0;
 
+    /**
+     * Recupere le nombre d'ouvrier du joueur
+     * @return le nombre d'ouvrier
+     */
     public int getNbOuvrier() {
         return nbOuvrier;
     }
 
+    /**
+     * Assigner un nombre d'ouvrier au joueur
+     * @param nbOuvrier Le nombre d'ouvrier
+     */
     public void setNbOuvrier(int nbOuvrier) {
         this.nbOuvrier = nbOuvrier;
     }
 
+    /**
+     * Soustrer un nombre d'ouvrier au joueur qu'il veut placer sur une zone
+     * @param nbOuvrier Le nombre d'ouvrier
+     */
     public void subOuvrier(int nbOuvrier){ this.nbOuvrier -= nbOuvrier; }
 
+    /**
+     * Recuperer le nombre de ressource du joueur
+     * @return Le nombre de ressource
+     */
     public int getNbRessource() {
         return nbRessource;
     }
 
+    /**
+     * Assigne le nombre de ressource au joueur
+     * @param nbRessource Le nombre de ressource
+     */
     public void setNbRessource(int nbRessource) {
         this.nbRessource = nbRessource;
     }
 
+    /**
+     * Rajouter 1 ressource a l'inventaire du joueur
+     */
     public void addRessource(){ this.nbRessource++; }
 
+    /**
+     * Constructeur de la classe, on assigne 1 ouvrier par default
+     */
     public Inventaire(){
         setNbOuvrier(1);
     }
 
+    /**
+     * Constructeur de la classe avec 2 parametres
+     * @param nOuvrier Le nombre d'ouvrier a assigner
+     * @param nRessource Le nombre de ressource a assigner
+     */
     public Inventaire(int nOuvrier, int nRessource){
         setNbRessource(nRessource);
         setNbOuvrier(nOuvrier);
