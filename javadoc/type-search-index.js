b01d21c40f863c153076ad56533a6de75b330e00
Mohamed06000 <mazymohamed06@gmail.com>
2019-11-03 01:13:01 +0100

    add javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"stoneage","l":"Inventaire"},{"p":"stoneage","l":"Joueur"},{"p":"stoneage","l":"Main"},{"p":"stoneage","l":"Zone"}]
