051ce81fc17337bea87def587d364746d648d0c2
Said Elarays <saidelarays@hotmail.fr>
2019-12-01 15:50:06 +0100

    test

--- stone_age/src/test/java/stoneage/ZoneTest.java
+++ stone_age/src/test/java/stoneage/ZoneTest.java
@@ -1,26 +1,5 @@
-package stoneage;
-
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ZoneTest {
 
-    @Test
-    void placeOuvrierSurZone() {
-
-    }
-
-    @Test
-    void retirerOuvrierSurZone() {
-    }
-
-    @Test
-    void procedure() {
-    }
-
-    @Test
-    void gainZone() {
-
-    }
 }
