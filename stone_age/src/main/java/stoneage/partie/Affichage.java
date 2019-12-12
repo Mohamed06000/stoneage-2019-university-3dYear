b68f1de73fc843710316b98f84fbe3d979e41544
Jaguar16 <aymen.baroudi@etu.unice.fr>
2019-12-12 01:29:12 +0100

    Ajout de la documentation dans la classe Affichage

--- stone_age/src/main/java/stoneage/partie/Affichage.java
+++ stone_age/src/main/java/stoneage/partie/Affichage.java
@@ -4,18 +4,23 @@ import stoneage.plateaudejeu.Inventaire;
 import stoneage.plateaudejeu.Ressource;
 import stoneage.plateaudejeu.cartes.Cartebatiment;
 import stoneage.plateaudejeu.zones.Zone;
-
 import java.util.ArrayList;
-import java.util.logging.SocketHandler;
 
+/**
+ * Classe Affichage, classe qui affiche les messages d'information sur l'avancée du jeu
+ * Les placements, les lancés de dé, etc
+ */
 public class Affichage {
     private boolean affichage;
 
+
+    /**
+     * Constructeur de la classe Affichage determine si l'affichage se fait ou non
+     * @param affichage
+     */
     public Affichage(boolean affichage) {
         this.affichage = affichage;
     }
-
-
 
     /**
      * Affiche les informations du joueur numero numJ
@@ -94,6 +99,12 @@ public class Affichage {
     }
 
 
+    /**
+     * Affiche un message avec le Tour dans lequel on est et le premier joueur qui va commencer le tour
+     * Affiche également un message qui informe qu'on entre dans la première phase de jeu à savoir les placements.
+     * @param nbTour
+     * @param premierAjouer
+     */
     public void AfficheInfoTour(int nbTour, int premierAjouer){
         if (this.affichage){
             System.out.println("_________________________________________________");
@@ -105,7 +116,9 @@ public class Affichage {
     }
 
 
-
+    /**
+     * Affiche un message d'information lorsqu'on entre dans la deuxième phase de jeu, lorsque les joueurs récupèrent et utilisent leurs ouvriers.
+     */
     public void AfficheRecup(){
         if (this.affichage){
             System.out.println(" ");
@@ -113,6 +126,9 @@ public class Affichage {
         }
     }
 
+    /**
+     *Affiche un message d'information lorsqu'on entre dans la phase de jeu où les joueurs nourrissent leurs ouvriers.
+     */
     public void AfficheNourrir(){
         if (this.affichage){
             System.out.println(" ");
@@ -120,6 +136,11 @@ public class Affichage {
         }
     }
 
+    /**
+     * Affiche le resultat obtenu après le lancé de dé du joueur numero numJ
+     * @param numJ
+     * @param resultat
+     */
     public void AfficheLanceDe(int numJ, int resultat){
         if (this.affichage){
             System.out.println("Le Joueur " + numJ + " lance le dé");
@@ -127,12 +148,22 @@ public class Affichage {
         }
     }
 
+    /**
+     * Affiche le gain final après les lancés de dés.
+     * @param somme
+     * @param diviseur
+     * @param gain
+     */
     public void AfficheGain(int somme, int diviseur,int gain){
         if (this.affichage) {
             System.out.println("Il a un gain de  "+ somme +" divisé par " + diviseur + " soit : " + gain);
         }
     }
 
+    /**
+     * Affiche le numero du joueur.
+     * @param numJ
+     */
     public void AffichageNumJoueur(int numJ){
         if (this.affichage){
             System.out.println(" ");
@@ -140,6 +171,11 @@ public class Affichage {
         }
     }
 
+    /**
+     * Affiche un message qui informe du placement d'ouvriers du joueur numJ sur la Zone Z.
+     * @param numJ
+     * @param z
+     */
     public void AffichePlacement(int numJ, Zone z){
         if (this.affichage){
             if (z.getNbOuvirerDuJoueur(numJ)>1){
@@ -151,6 +187,10 @@ public class Affichage {
         }
     }
 
+    /**
+     * Affiche le message du placement sur une carte.
+     * @param c
+     */
     public void AffichePlacement(Cartebatiment c){
         if (this.affichage){
             System.out.println("Il place un ouvrier sur la Carte "+ c);
@@ -158,7 +198,11 @@ public class Affichage {
     }
 
 
-
+    /**
+     * Affiche le message d'utilisation des ouvriers par le joueur de numero numJ sur une Zone z.
+     * @param numJ
+     * @param z
+     */
     public void AfficheRecup(int numJ, Zone z){
         if (this.affichage){
             if (z.getNbOuvirerDuJoueur(numJ)>1){
@@ -171,19 +215,33 @@ public class Affichage {
     }
 
 
+    /**
+     * Affiche le message d'utilisation des ouvriers sur une Carte Batiment c.
+     * @param c
+     */
     public void AfficheRecup(Cartebatiment c){
         if (this.affichage) {
             System.out.println("Il utilise son ouvrier placé sur la Carte " + c);
         }
     }
 
-
+    /**
+     * Affiche le message du nombre de ressource que les ouvriers du joueurs ont produit
+     * @param gain
+     * @param ressourceDeLaZone
+     */
     public void AfficheGainRessource(int gain, Ressource ressourceDeLaZone) {
         if (this.affichage) {
             System.out.println("Il produit " + gain + " " + ressourceDeLaZone);
         }
     }
 
+    /**
+     * Affiche le message du gain d'un niveau d'agriculture lorsque type_zone=0,
+     * un outils lorsque type_zone=1,
+     * un ouvrier autrement.
+     * @param type_zone
+     */
     public void AfficheGainVillage(int type_zone){ /* 0 est le champ*/
         if (this.affichage) {
             if (type_zone == 0) {
@@ -197,6 +255,9 @@ public class Affichage {
     }
 
 
+    /**
+     * Affiche le message de Fin de partie
+     */
     public void AfficheMessageFin() {
         if (this.affichage) {
             System.out.println(" ");
